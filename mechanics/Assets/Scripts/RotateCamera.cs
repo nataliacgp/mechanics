744663f9b3023b0af7beaa3799ce9ccb6ab1fc4b
natycg <checon@roverkids.org>
2025-01-06 13:56:26 -0500

    game object

--- mechanics/Assets/Scripts/RotateCamera.cs
+++ mechanics/Assets/Scripts/RotateCamera.cs
@@ -15,6 +15,7 @@ public class RotateCamera : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
