e7300fa4f7e5aad3177312d89f9e3b3268bb5d1a
natycg <checon@roverkids.org>
2025-01-08 13:25:58 -0500

    Apply extra knockback with powerup

--- mechanics/Assets/Scripts/PlayerController.cs
+++ mechanics/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool hasPowerup;
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        
+
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
 }
+
+
+       
